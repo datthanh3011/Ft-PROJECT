fb0a0b7b20377d15d5feb23e8b45b0d8bd569104
Thanh <ThanhTTD@scavibh.local>
2020-05-23 21:48:40 +0700

    front-end login

--- index.php
+++ index.php
@@ -1,10 +1,40 @@
 <html>
+
 <head>
-	<meta charshet="utf-8" />
+    <meta charshet="utf-8" />
+    <link rel="stylesheet" href="css/css.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
-	<h1>
-		<?php
-		echo "Web Bán Hàng";
-		?>
-	</h1>
+
+<body>
+    <div id="bg-header-login">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-6">Chào Mừng bạn Đến với ReVo!</div>
+                <div class="col-md-6 col-sm-6 col-xs-6 ">
+                    <div class ="row flex-container">
+						<div class="margin-div">
+							<span>
+								Đăng nhập
+							</span>
+						</div>
+						<div class="margin-div">
+							<span>
+								Đăng kí
+							</span>
+						</div>
+					</div>
+                </div>
+            </div>
+        </div>
+	</div>
+
+	<div class="container">
+		<div class="col-xs-3 hiden-xs">
+			
+		</div>
+	</div>
+</body>
+
 </html>
