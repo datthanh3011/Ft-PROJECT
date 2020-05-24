ccedaf783149add70e4f781b37c6a129c613f72f
Thanh <ThanhTTD@scavibh.local>
2020-05-24 14:10:33 +0700

    chuyen code sang scss

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <head>
     <meta charshet="utf-8" />
-    <link rel="stylesheet" href="css/css.css">
+    <link rel="stylesheet" href="sass/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
@@ -14,12 +14,12 @@
                 <div class="col-md-6 col-sm-6 col-xs-6">Chào Mừng bạn Đến với ReVo!</div>
                 <div class="col-md-6 col-sm-6 col-xs-6 ">
                     <div class ="row flex-container">
-						<div class="margin-div">
+						<div>
 							<span>
 								Đăng nhập
 							</span>
 						</div>
-						<div class="margin-div">
+						<div>
 							<span>
 								Đăng kí
 							</span>
@@ -30,9 +30,16 @@
         </div>
 	</div>
 
-	<div class="container">
-		<div class="col-xs-3 hiden-xs">
-			
+	<div id="bg-mg-logo">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-3 hiden-md">
+					<img src="img/logo.png">
+				</div>
+				<div class="col-xs-6 col-md-9 hiden-sm " id="txt-seach">
+					<input type="text" >
+				</div>
+			</div>
 		</div>
 	</div>
 </body>
