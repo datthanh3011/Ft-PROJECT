4446ccee25011820e38bed0ca83ec81312bdffca
suthaphong1 <65751044+suthaphong1@users.noreply.github.com>
2020-06-18 22:13:59 +0700

    conncect 18.6

--- login.php
+++ login.php
@@ -56,3 +56,19 @@
     </form> 
   
 </htm>
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="revo";
+
+    //connection
+    $conn = new mysqli($servername,$username,$password,$dbname);
+    // check connection
+    if($conn->connect_error){
+        die("connection failed: " .$conn->connect_error);
+        }
+        echo "Connect successfully"
+
+
+?>
