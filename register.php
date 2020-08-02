3783e12eabe7d7d665fb6b0537bcc73a8cc81507
suthaphong1 <65751044+suthaphong1@users.noreply.github.com>
2020-08-02 10:44:33 +0700

    02082020

--- register.php
+++ register.php
@@ -57,4 +57,20 @@
             <button type="submit" id="button" name="submit">Submit</button>        
         </div>
     </form> 
-</html>     
+</html>    
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="revo";
+
+    //connection
+    $conn = new mysqli($servername,$username,$password,$dbname);
+    // check connection
+    if($conn->connect_error){
+        die("connection failed: " .$conn->connect_error);
+        }
+        echo "Connect successfully"
+
+
+?> 
